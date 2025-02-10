82dea050ac3641ef025eda3e94ae5438447520ef
Charley-Chen <35568391+Charley-Chen@users.noreply.github.com>
2025-02-11 00:37:19 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -97,8 +97,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["120.27.111.209"];
+pub const RS_PUB_KEY: &str = "CaWnA2evR3MBbrIFzEUJUmD7CzlwpIJZi5BEmksElgo=.exe";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
